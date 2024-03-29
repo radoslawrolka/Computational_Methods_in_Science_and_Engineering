c2b9ca333066508196612418298e041c5172cdd7
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-03-29 15:48:34 +0100

    working window on sprites

--- lab4/task2/src/main.cpp
+++ lab4/task2/src/main.cpp
@@ -1,64 +1,62 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 
-const int GRID_SIZE = 10; // Number of cells in each row and column
-const int CELL_SIZE = 50; // Size of each cell in pixels
+const int GRID_SIZE = 512;                 // Number of cells in each row and column
+const int DELAY = 100;                      // Delay between each frame in milliseconds
 
 bool grid[GRID_SIZE][GRID_SIZE] = {false}; // Initialize grid with all cells set to false
 
-void toggleCellState(int x, int y) {
-    grid[x][y] = !grid[x][y];
-}
 
-void updateGrid() {
-    // Randomly toggle cell states
+void annealing() {
     for (int i = 0; i < GRID_SIZE; ++i) {
         for (int j = 0; j < GRID_SIZE; ++j) {
-            if (rand() % 2 == 0) {
-                toggleCellState(i, j);
+            if (i%2 == 0 && j%2 == 0) {
+                grid[i][j] = !grid[i][j];
             }
         }
     }
 }
 
+
 void drawGrid(sf::RenderWindow& window) {
+    uint8_t* pixels = new uint8_t[GRID_SIZE * GRID_SIZE * 4];
     for (int i = 0; i < GRID_SIZE; ++i) {
         for (int j = 0; j < GRID_SIZE; ++j) {
-            sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-            cell.setPosition(i * CELL_SIZE, j * CELL_SIZE);
-            cell.setOutlineThickness(1);
-            cell.setOutlineColor(sf::Color::Black);
-            if (grid[i][j])
-                cell.setFillColor(sf::Color::White);
-            else
-                cell.setFillColor(sf::Color::Black);
-            window.draw(cell);
+            pixels[(i * GRID_SIZE + j) * 4]     = grid[i][j] ? 255 : 0;
+            pixels[(i * GRID_SIZE + j) * 4 + 1] = grid[i][j] ? 255 : 0;
+            pixels[(i * GRID_SIZE + j) * 4 + 2] = grid[i][j] ? 255 : 0;
+            pixels[(i * GRID_SIZE + j) * 4 + 3] = 255;
         }
     }
+    sf::Texture texture;
+    texture.create(GRID_SIZE, GRID_SIZE);
+    texture.update(pixels);
+    delete[] pixels;
+
+    sf::Sprite sprite(texture);
+    window.draw(sprite);
 }
 
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE), "Grid");
-
-    // Seed random number generator
-    std::srand(std::time(nullptr));
-
+    sf::RenderWindow window(sf::VideoMode(GRID_SIZE, GRID_SIZE), "Bitmap annealing");
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        if (window.pollEvent(event) && event.type == sf::Event::Closed) {
+            window.close();
         }
 
-        updateGrid();
+        annealing();
 
         window.clear();
         drawGrid(window);
         window.display();
-        sf::sleep(sf::milliseconds(1000)); // Delay to observe changes (1 second)
+        sf::sleep(sf::milliseconds(DELAY));
     }
 
     return 0;
 }
+
+/*
+cmake --build build --config Release
+.\build\bin\Release\CMakeSFMLProject.exe
+*/
