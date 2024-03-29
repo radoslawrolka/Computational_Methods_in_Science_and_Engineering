0864d375e903fae6fa13122666a7fce104a9aedd
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-03-29 21:54:15 +0100

    all done, examples to render

--- lab4/task2/src/main.cpp
+++ lab4/task2/src/main.cpp
@@ -7,9 +7,11 @@
 #include <ctime>
 
 // Global 
-const int GRID_SIZE = 512;                 // Number of cells in each row and column
+const int GRID_SIZE = 10;                 // Number of cells in each row and column
 const int DELAY = 0;                       // Delay between each frame in milliseconds
 bool grid[GRID_SIZE][GRID_SIZE] = {false}; // Initialize grid with all cells set to false
+std::random_device rd;
+std::mt19937 g(rd());
 
 // Function prototypes
 struct Points {int x1, y1, x2, y2;};
@@ -24,20 +26,20 @@ void initializeGrid();
 
 // Energy models (if grid[i][j] == grid[i+k][j+l] then energy += surround_by_same[k+1][l+1])
 const int chess_board[3][3] = {{-1, 1, -1}, {1, 0, 1}, {-1, 1, -1}};
-const int group[3][3]   = {{1, -1, 1}, {-1, 0, -1}, {1, -1, 1}};
+const int group[3][3]   = {{-1, -1, -1}, {-1, 0, -1}, {-1, -1, -1}};
 const int vertical[3][3] = {{1, -1, 1}, {1, 0, 1}, {1, -1, 1}};
-// TODO
-
+const int horizontal[3][3] = {{1, 1, 1}, {-1, 0, -1}, {1, 1, 1}};
+const int diagonal[3][3] = {{-10, 5, 10}, {-5, 0, -5}, {10, 5, -10}};
 
 // Parameters
 const float density = 0.5;                 // Density of the grid (black cells)
 double temperature = 1;                    // Initial temperature
-const double cooling_rate = 0.999;          // Cooling rate
-const float acceptance = 50;              // Acceptance probability
+const double cooling_rate = 0.999;         // Cooling rate
+const float acceptance = 50;               // Acceptance probability
 const int iterations = 10000;              // Number of iterations
-const int* model = vertical[0];    // Energy model
-const int nrows = 3;                       // Number of rows in the energy model
-const int ncols = 3;                       // Number of columns in the energy model
+const int* model = group[0];              // Energy model
+const int nrows = 5;                       // Number of rows in the energy model
+const int ncols = 5;                       // Number of columns in the energy model
 
 int main() {
     srand(time(NULL));
@@ -59,25 +61,32 @@ int main() {
     return 0;
 }
 
-
-int energyDifference(Points points) {
+int pointEnergy(int x, int y) {
     int energy = 0;
     for (int offset_x = -1; offset_x <= 1; ++offset_x) {
         for (int offset_y = -1; offset_y <= 1; ++offset_y) {
-            int x1 = points.x1 + offset_x;
-            int y1 = points.y1 + offset_y;
-            int x2 = points.x2 + offset_x;
-            int y2 = points.y2 + offset_y;
+            int x1 = x + offset_x;
+            int y1 = y + offset_y;
             if (x1 >= 0 && x1 < GRID_SIZE && y1 >= 0 && y1 < GRID_SIZE) {
-                // Calculate index for model array
-                int index1 = (offset_x + 1) * 3 + (offset_y + 1);
-                energy += model[index1] * (grid[points.x1][points.y1] == grid[x1][y1] ? 1 : -1);
+                int index = (offset_x + 1) * ncols + (offset_y + 1);
+                energy += model[index] * (grid[x][y] == grid[x1][y1] ? 1 : -1);
             }
-            if (x2 >= 0 && x2 < GRID_SIZE && y2 >= 0 && y2 < GRID_SIZE) {
-                // Calculate index for model array
-                int index2 = (offset_x + 1) * 3 + (offset_y + 1);
-                energy += model[index2] * (grid[points.x2][points.y2] == grid[x2][y2] ? 1 : -1);
-            }
+        }
+    }
+    return energy;
+}
+
+int energyDifference(Points points) {
+    int energy = 0;
+    //count energy of the points and the points around them
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            int x1 = points.x1 + i;
+            int y1 = points.y1 + j;
+            int x2 = points.x2 + i;
+            int y2 = points.y2 + j;
+            energy += pointEnergy(x1, y1);
+            energy += pointEnergy(x2, y2);
         }
     }
     return energy;
@@ -93,21 +102,16 @@ std::vector<std::pair<int, int>> generatePermutations(int nrows, int ncols) {
     return permutations;
 }
 
-void shufflePermutations(std::vector<std::pair<int, int>>& permutations) {
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(permutations.begin(), permutations.end(), g);
-}
-
 void initializeGrid() {
     std::vector<std::pair<int, int>> permutations = generatePermutations(GRID_SIZE, GRID_SIZE);
-    shufflePermutations(permutations);
+    std::shuffle(permutations.begin(), permutations.end(), g);
     for (int i = 0; i < int(GRID_SIZE*GRID_SIZE*density); ++i) {
         grid[permutations[i].first][permutations[i].second] = true;
     }
 }
 
 Points RandomPoints() {
+    // use random_device to get a random number
     return Points{rand() % GRID_SIZE, rand() % GRID_SIZE, rand() % GRID_SIZE, rand() % GRID_SIZE};
 }
 
@@ -123,20 +127,14 @@ int countEnergy() {
     int energy = 0;
     for (int i = 0; i < GRID_SIZE; ++i) {
         for (int j = 0; j < GRID_SIZE; ++j) {
-            for (int offset_x = -1; offset_x <= 1; ++offset_x) {
-                for (int offset_y = -1; offset_y <= 1; ++offset_y) {
-                    int x = i + offset_x;
-                    int y = j + offset_y;
-                    if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
-                        int index = (offset_x + 1) * 3 + (offset_y + 1);
-                        energy += model[index] * (grid[i][j] == grid[x][y] ? 1 : -1);
-                    }
-                }
-            }
+            energy += pointEnergy(i, j);
         }
     }
     return energy;
 }
+
+
+
 void annealing(int& energy) {
     temperature *= cooling_rate;
     Points points = RandomPoints();
